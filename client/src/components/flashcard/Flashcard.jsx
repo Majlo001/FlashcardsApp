b52dbe11302712ca1fc6134f5eed31d6216bb1d1
Majlo001 <szmilosz01@gmail.com>
2022-11-29 17:42:57 +0100

    FlashcardsApp 0.0.3

--- client/src/components/flashcard/Flashcard.jsx
+++ client/src/components/flashcard/Flashcard.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import "./flashcard.scss"
+import pencilIcon from "../../assets/icons/pencil-solid.svg"
+import starIcon from "../../assets/icons/star-solid.svg"
+import volumeIcon from "../../assets/icons/volume.svg"
 
 const Flashcard = ({frontText, backText}) => {
     const [isFlipped, setIsFlipped] = useState(false);
 
-    const handleKeyDown = (event) => {
+    const handleKeyDownFlashcard = (event) => {
         const { key, keyCode } = event;
         if(key === 'ArrowUp' || key === 'ArrowDown' || key === 'Space'){
             //console.log('clicked: ', key)
@@ -17,28 +19,40 @@ const Flashcard = ({frontText, backText}) => {
     };
 
       
-    React.useEffect(() => {
-        window.addEventListener('keydown', handleKeyDown); //Może zmienić na keyup
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDownFlashcard);
 
         // cleanup this component
         return () => {
-            window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keydown', handleKeyDownFlashcard);
         };
-    }, [handleKeyDown]);
+    }, [handleKeyDownFlashcard]);
 
     return (
-        <div className="flashcard__wrapper w-[500px] h-[420px] cursor-pointer" onClick={() => setIsFlipped(!isFlipped)}>
+        <div className="flashcard__wrapper w-[800px] h-[560px] cursor-pointer" onClick={() => setIsFlipped(!isFlipped)}>
             <div className={`flashcard__wrapper-inner w-full h-full text-center ${isFlipped ? "flashcard__wrapper-inner-rotate" : ""}`}>
-                <div className="flashcard__front absolute w-full h-full bg-gray-100">
-                    <div className="flashcard__top-panel flex w-full h-[40px]">
-
+                <div className="flashcard__front absolute w-full h-full bg-gray-100 rounded-3xl">
+                    <div className="flashcard__top-panel flex justify-between w-full h-[64px] py-4 px-8">
+                        <div className="flashcard__top-panel-left">
+                            <div className="flashcard__top-panel-icon h-6 mr-4 cursor-pointer" onClick={() => {console.log("Edit")}}>
+                                <img src={volumeIcon} alt="volume" className="h-8"/>
+                            </div>
+                        </div>
+                        <div className="flashcard__top-panel-right flex flex-column">
+                            <div className="flashcard__top-panel-icon h-6 cursor-pointer" onClick={() => {console.log("Edit")}}>
+                                <img src={pencilIcon} alt="edit" className="h-6"/>
+                            </div>
+                            <div className="flashcard__top-panel-icon ml-4 h-6 cursor-pointer" onClick={() => {console.log("Star")}}>
+                                <img src={starIcon} alt="star" className="h-6"/>
+                            </div>
+                        </div>
                     </div>
-                    {frontText}
-                    <div className="flashcard__bottom-panel flex w-full h-[40px]">
+                    <h1 className="flashcard__text">{frontText}</h1>
+                    <div className="flashcard__bottom-panel flex w-full h-[48px]">
                         
                     </div>
                 </div>
-                <div className="flashcard__back absolute w-full h-full bg-gray-100">
+                <div className="flashcard__back absolute w-full h-full bg-gray-100 rounded-3xl">
                     {backText}
                 </div>
             </div>
--- client/src/components/flashcardDeck/FlashcardDeck.jsx
+++ client/src/components/flashcardDeck/FlashcardDeck.jsx
@@ -1,6 +1,28 @@
+import { useState, useEffect, useCallback } from "react";
 import Flashcard from '../flashcard/Flashcard'
 
 const FlashcardDeck = () => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleKeyDown = (event) => {
+        const { key, keyCode } = event;
+        if(key === 'ArrowLeft' || key === 'ArrowRight'){
+            //console.log('clicked: ', key)
+            const timer = setTimeout(()=> {
+                setIsFlipped(!isFlipped)
+            }, 200);
+            return ()=> clearTimeout(timer);
+        }
+    };
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown); //Może zmienić na keyup
+
+        // cleanup this component
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [handleKeyDown]);
+
     return(
         <div className="flashcard_deck__wrapper bg-gray-900 h-screen w-screen flex justify-center items-center">
             <Flashcard frontText="window" backText="okno"/>
