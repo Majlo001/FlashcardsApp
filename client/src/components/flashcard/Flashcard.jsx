14b27ca1cf85b9662987c984808c02fd97d41b37
Majlo001 <szmilosz01@gmail.com>
2022-11-29 12:31:01 +0100

    FlashcardsApp 0.0.2

--- client/src/components/flashcard/Flashcard.jsx
+++ client/src/components/flashcard/Flashcard.jsx
@@ -1,15 +1,49 @@
-import { useState } from "react";
+import React from 'react';
+import { useState, useEffect, useCallback } from "react";
 import "./flashcard.scss"
 
 const Flashcard = ({frontText, backText}) => {
-      const [isFlipped, setIsFlipped] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
 
-      return (
-            <div className="flashcard__wrapper w-[500px] h-[420px] cursor-pointer bg-gray-100" onClick={setIsFlipped(!isFlipped)}>
-                  <div className="flashcard__front">{frontText}</div>
-                  <div className="flashcard__back">{backText}</div>
+    const handleKeyDown = (event) => {
+        const { key, keyCode } = event;
+        if(key === 'ArrowUp' || key === 'ArrowDown' || key === 'Space'){
+            //console.log('clicked: ', key)
+            const timer = setTimeout(()=> {
+                setIsFlipped(!isFlipped)
+            }, 200);
+            return ()=> clearTimeout(timer);
+        }
+    };
+
+      
+    React.useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown); //Może zmienić na keyup
+
+        // cleanup this component
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [handleKeyDown]);
+
+    return (
+        <div className="flashcard__wrapper w-[500px] h-[420px] cursor-pointer" onClick={() => setIsFlipped(!isFlipped)}>
+            <div className={`flashcard__wrapper-inner w-full h-full text-center ${isFlipped ? "flashcard__wrapper-inner-rotate" : ""}`}>
+                <div className="flashcard__front absolute w-full h-full bg-gray-100">
+                    <div className="flashcard__top-panel flex w-full h-[40px]">
+
+                    </div>
+                    {frontText}
+                    <div className="flashcard__bottom-panel flex w-full h-[40px]">
+                        
+                    </div>
+                </div>
+                <div className="flashcard__back absolute w-full h-full bg-gray-100">
+                    {backText}
+                </div>
             </div>
-      );
+        </div>
+    );
 };
 
 export default Flashcard;
